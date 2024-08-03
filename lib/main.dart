9bb6a428f57e4f606e6f5300ed2d1cc9d044520e
Ahmed <01099517574med@gmail.com>
2024-08-04 02:08:37 +0300

    adding device preview

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,13 @@
 import 'package:bookly_app/Features/splash/presentation/views/splash_view.dart';
 import 'package:bookly_app/constant.dart';
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 void main() {
-  runApp(const BooklyApp());
+  runApp(DevicePreview(
+    enabled: true,
+    builder: (context)=> BooklyApp()));
 }
 
 class BooklyApp extends StatelessWidget {
@@ -13,6 +16,8 @@ class BooklyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
+      locale: DevicePreview.locale(context),
+      builder: DevicePreview.appBuilder,
       debugShowCheckedModeBanner: false,
       theme: ThemeData().copyWith(scaffoldBackgroundColor: KprimaryColor),
       home: const SplashViews(),
