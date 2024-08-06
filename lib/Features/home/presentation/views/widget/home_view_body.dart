a2c986e56aa61976a67e055e749ab1b6d321d996
Ahmed <01099517574med@gmail.com>
2024-08-06 19:28:12 +0300

    add custom abb bar to home

---
+++ lib/Features/home/presentation/views/widget/home_view_body.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/home/presentation/views/widget/custom_app_bar.dart';
+import 'package:bookly_app/core/utilis/assets.dart';
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class HomeViewBody extends StatelessWidget {
+  const HomeViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const CustomAppBar();
+  }
+}
