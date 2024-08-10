183690a9253197747ce4d0cfb157f13dccfccd5c
Ahmed <01099517574med@gmail.com>
2024-08-10 19:24:21 +0300

    create Book Details .dart

---
+++ lib/Features/home/presentation/views/book_details_views.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/Features/home/presentation/views/widget/book_details_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetails extends StatelessWidget {
+  const BookDetails({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailsViewBody(),
+    );
+  }
+}
