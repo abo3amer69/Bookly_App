e5dfb0c12b0cad8e87213d2df9fabf39134cbdf9
Ahmed <01099517574med@gmail.com>
2024-08-11 18:55:27 +0300

    create custom search text field

--- lib/Features/search/presentation/views/widget/search_view_body.dart
+++ lib/Features/search/presentation/views/widget/search_view_body.dart
@@ -1,10 +1,16 @@
+import 'package:bookly_app/Features/search/presentation/views/widget/custom_search_text_field.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class SearchViewBody extends StatelessWidget {
   const SearchViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const Column(
+      children: [
+        CustomSearchTextField(),
+      ],
+    );
   }
 }
