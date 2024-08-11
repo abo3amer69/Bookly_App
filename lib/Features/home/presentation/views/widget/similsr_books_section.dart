81e0a4f11d82ee7c9311adf0442ead4dfe2f3b8f
Ahmed <01099517574med@gmail.com>
2024-08-11 16:49:18 +0300

    refactor book details view

---
+++ lib/Features/home/presentation/views/widget/similsr_books_section.dart
@@ -0,0 +1,27 @@
+
+import 'package:bookly_app/Features/home/presentation/views/widget/similar_books_list_view.dart';
+import 'package:bookly_app/core/utilis/styles.dart';
+import 'package:flutter/material.dart';
+
+class SimilsrBooksSection extends StatelessWidget {
+  const SimilsrBooksSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'You can also like',
+          style: Styles.textStyle14.copyWith(
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        const SizedBox(
+          height: 16,
+        ),
+        const similarBooksListView(),
+      ],
+    );
+  }
+}
